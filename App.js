4d87f46eb59cfd2ec1d5ee0de78f3ac475039397
mshrestha5 <104951387+mshrestha5@users.noreply.github.com>
2022-09-13 19:41:46 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My todos</h1>
+      <Todo text="Learn react" />
+      <Todo text="master react" />
+      <Todo text="Explore the full React course" />
+    </div>
+  );
+}
+
+export default App;
